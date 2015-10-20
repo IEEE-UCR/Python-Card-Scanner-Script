505cc4505f586324f0c2bdd4d830765774baa0b6
csimo005 <csimo005@ucr.edu>
2015-10-19 19:42:10 -0700

    comitt for daniel

--- card_scanner_script.py
+++ card_scanner_script.py
@@ -4,6 +4,20 @@ import datetime
 import os
 import sys
 
+def swipeLinux():
+	os.system("stty -echo")
+	input = raw_input()
+	os.system("stty echo")
+	return input
+
+def genEmail(fname, lname, digits):
+	address = fname[0]
+	lnamelength = len(lname)
+	i = 0
+	added = 0
+	while i < 0 && i < lnamelength:
+		 
+
 #open output file
 fo = open("parsed.txt", "w")
 if(fo.closed):
@@ -13,9 +27,12 @@ raw = open("raw.txt", "w")
 if(raw.closed):
    print "Error: file failed to open."
    sys.exit()
+
 #declare and initalize variables
-swipe = "swipe"
-name = "John Doe"
+track1 = "t1"
+track2 = "t2"
+fname = "John"
+lname = "Doe"
 cardNum = "8675309"
 idnum = "123"
 slash = 1
@@ -23,35 +40,41 @@ space = 1
 carrot = 1
 outPut = "foo"
 
-os.system("stty -echo")
-swipe = raw_input()
-os.system("stty echo")
+#takes care of os dependent stuff
+if(sys.platform == 'linux2'):
+	track1 = swipeLinux()
+	track2 = swipeLinux()
+	
 #while not exiting
-while(swipe != ""):
+while(track1 != ""):
 	#check if read is succesfull
-	if(swipe[1] == 'B'):
+	if(track1[1] == 'B'):
 		#parse input to multiple strings
-		cardNum = swipe[2:18]
-		slash = swipe.find('/')
-		space = swipe.find('  ') + 2
-		carrot = swipe.find(' ^')
-		name = swipe[slash  + 1:space] + swipe[19:slash]
-		idnum = swipe[carrot+14:carrot+23]
+		cardNum = track1[2:18]
+		slash = track1.find('/')
+		space = track1.find('  ') + 2
+		carrot = track1.find(' ^')
+		fname = track1[slash  + 1:space] 
+		lname = track1[19:slash]
+		idnum = track1[carrot+14:carrot+23]
 		#output that in the format for the file
 		ts = time.time()
 		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+		print("Welcome " + fname + " " + lname + "!")
+		digits = input("Please input the three digits at the end of your netID: ")
+		email
 		outPut = "Name:" + '\t' + name + '\t' + "Card Number:" + '\t' + cardNum + '\t' + "ID number:"+ '\t' + idnum + '\t' + "Time:" + '\t' + st + '\n'
 		fo.write(outPut)
-		raw.write(swipe)
-		print("Welcome " + name + "!")
-	elif(swipe[1] == 'E'):
+		raw.write(track1 + track2)
+	elif(track1[1] == 'E'):
 		#output error message
 		print("Error: Please reswipe card")	   
 	#next swipe
-	os.system("stty -echo")
-	swipe = raw_input()
-	os.system("stty echo")
+	if(sys.platform == "linux2"):
+        	track1 = swipeLinux()
+        	track2 = swipeLinux()
 #end while
 #close output file
 fo.close()
+raw.close()
 #end program
